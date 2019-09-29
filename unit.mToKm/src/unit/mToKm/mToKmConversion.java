060c5687fa99b4375b4da83c850f0dae71a960d7
Mats Elias Davidsen <31271640+matseda@users.noreply.github.com>
2019-09-29 17:15:47 +0200

    Update mToKmConversion.java

--- unit.mToKm/src/unit/mToKm/mToKmConversion.java
+++ unit.mToKm/src/unit/mToKm/mToKmConversion.java
@@ -7,7 +7,7 @@ import unit.api.Conversion;
 @Component(
 		property = {
 				UnitConversion.CONVERSION_NAME_PROP + "=mToKm",
-				UnitConversion.CONVERSION_FORMULA_PROP + "=*1000"}
+				UnitConversion.CONVERSION_FORMULA_PROP + "=/1000"}
 		)
 public class mToKmConversion extends UnitConversion implements Conversion {
 }
